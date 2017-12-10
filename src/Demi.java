8bbc65fd4ff998e28bd6e4a70bba58749ac79123
lirong <lirongmvp@163.com>
2017-12-10 22:53:59 +0800

    提交到github

---
+++ src/Demi.java
@@ -0,0 +1,13 @@
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈asd〉
+ *
+ * @author lirong
+ * @create 2017/12/10
+ * @since 1.0.0
+ */
+public class Demi {
+    public static void main(String[] args) {
+        System.out.println("haa!");
+    }
+}
