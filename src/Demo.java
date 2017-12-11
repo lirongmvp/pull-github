9d0296c97ab2b752d8fb501b5e563339da6ac183
lirong <lirongmvp@163.com>
2017-12-11 15:36:31 +0800

    java demo1

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈aha 〉
+ *
+ * @author lirong
+ * @create 2017/12/11
+ * @since 1.0.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
